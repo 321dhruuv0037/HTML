14106819fac68ada48bd1607de96e6b73e436203
321dhruuv0037 <321dhruuv0037@dbit.in>
2023-01-01 21:30:47 +0530

    added wordle project

---
+++ wordle/script.js
@@ -0,0 +1,660 @@
+let words = [
+    "tower",
+    "topic",
+    "chart",
+    "terry",
+    "apart",
+    "bases",
+    "often",
+    "early",
+    "start",
+    "under",
+    "pride",
+    "worst",
+    "exist",
+    "money",
+    "peace",
+    "cable",
+    "exact",
+    "rapid",
+    "chest",
+    "sharp",
+    "after",
+    "strip",
+    "major",
+    "cause",
+    "think",
+    "adult",
+    "input",
+    "logic",
+    "while",
+    "draft",
+    "stage",
+    "fruit",
+    "ready",
+    "local",
+    "nurse",
+    "judge",
+    "water",
+    "cheap",
+    "floor",
+    "scene",
+    "sized",
+    "these",
+    "blind",
+    "adopt",
+    "focus",
+    "doing",
+    "court",
+    "grant",
+    "death",
+    "alone",
+    "solve",
+    "giant",
+    "upset",
+    "fight",
+    "spent",
+    "delay",
+    "above",
+    "phase",
+    "other",
+    "maker",
+    "limit",
+    "brain",
+    "final",
+    "Angle",
+    "valid",
+    "urban",
+    "newly",
+    "stuff",
+    "theme",
+    "alike",
+    "pound",
+    "treat",
+    "birth",
+    "forty",
+    "bound",
+    "track",
+    "forum",
+    "event",
+    "sound",
+    "drill",
+    "guard",
+    "inner",
+    "point",
+    "their",
+    "chase",
+    "space",
+    "enter",
+    "chose",
+    "sugar",
+    "jones",
+    "women",
+    "audio",
+    "roman",
+    "asset",
+    "flash",
+    "faith",
+    "quite",
+    "royal",
+    "happy",
+    "blame",
+    "dance",
+    "legal",
+    "leave",
+    "going",
+    "scope",
+    "catch",
+    "fifth",
+    "worth",
+    "check",
+    "field",
+    "frame",
+    "pilot",
+    "union",
+    "first",
+    "quiet",
+    "grand",
+    "being",
+    "human",
+    "whole",
+    "house",
+    "throw",
+    "metal",
+    "storm",
+    "short",
+    "doubt",
+    "block",
+    "steam",
+    "curve",
+    "shift",
+    "black",
+    "lives",
+    "claim",
+    "video",
+    "class",
+    "right",
+    "tries",
+    "mount",
+    "prior",
+    "where",
+    "north",
+    "drive",
+    "clear",
+    "aware",
+    "laser",
+    "visit",
+    "train",
+    "build",
+    "guest",
+    "dated",
+    "value",
+    "again",
+    "allow",
+    "heavy",
+    "trust",
+    "calif",
+    "horse",
+    "image",
+    "solid",
+    "skill",
+    "bench",
+    "roger",
+    "force",
+    "abuse",
+    "undue",
+    "fiber",
+    "front",
+    "proof",
+    "shelf",
+    "theft",
+    "close",
+    "small",
+    "those",
+    "crash",
+    "panel",
+    "write",
+    "until",
+    "seven",
+    "needs",
+    "shoot",
+    "carry",
+    "rough",
+    "group",
+    "fluid",
+    "daily",
+    "grown",
+    "plain",
+    "prime",
+    "mixed",
+    "ratio",
+    "admit",
+    "wound",
+    "dress",
+    "buyer",
+    "sport",
+    "equal",
+    "coach",
+    "later",
+    "chief",
+    "laugh",
+    "alive",
+    "level",
+    "thank",
+    "worse",
+    "found",
+    "mayor",
+    "occur",
+    "speed",
+    "anger",
+    "dying",
+    "child",
+    "lower",
+    "craft",
+    "range",
+    "ideal",
+    "grass",
+    "dream",
+    "broad",
+    "novel",
+    "japan",
+    "cycle",
+    "cross",
+    "model",
+    "breed",
+    "meant",
+    "state",
+    "count",
+    "motor",
+    "match",
+    "issue",
+    "coast",
+    "white",
+    "alert",
+    "usage",
+    "lewis",
+    "heart",
+    "which",
+    "stock",
+    "reach",
+    "watch",
+    "angry",
+    "truly",
+    "worry",
+    "built",
+    "shell",
+    "brand",
+    "boost",
+    "crime",
+    "yield",
+    "sixth",
+    "split",
+    "henry",
+    "spend",
+    "rival",
+    "twice",
+    "funny",
+    "arena",
+    "chain",
+    "drama",
+    "route",
+    "joint",
+    "piece",
+    "today",
+    "stone",
+    "baker",
+    "stake",
+    "brown",
+    "board",
+    "night",
+    "extra",
+    "guess",
+    "every",
+    "price",
+    "cream",
+    "civil",
+    "slide",
+    "china",
+    "trade",
+    "shape",
+    "queen",
+    "paper",
+    "upper",
+    "peter",
+    "store",
+    "crowd",
+    "photo",
+    "press",
+    "bring",
+    "rural",
+    "style",
+    "globe",
+    "youth",
+    "fully",
+    "order",
+    "taxes",
+    "speak",
+    "steel",
+    "paint",
+    "sight",
+    "study",
+    "truth",
+    "party",
+    "taste",
+    "fixed",
+    "maybe",
+    "elite",
+    "phone",
+    "avoid",
+    "sweet",
+    "trial",
+    "smile",
+    "ahead",
+    "eight",
+    "sorry",
+    "least",
+    "month",
+    "lying",
+    "great",
+    "might",
+    "given",
+    "sense",
+    "three",
+    "badly",
+    "entry",
+    "beach",
+    "power",
+    "award",
+    "hence",
+    "ocean",
+    "since",
+    "blood",
+    "spare",
+    "staff",
+    "grace",
+    "trend",
+    "layer",
+    "gross",
+    "depth",
+    "lucky",
+    "stood",
+    "actor",
+    "teeth",
+    "spoke",
+    "drawn",
+    "agree",
+    "could",
+    "prove",
+    "lease",
+    "threw",
+    "river",
+    "whose",
+    "drink",
+    "bread",
+    "began",
+    "guide",
+    "aside",
+    "sheet",
+    "eager",
+    "place",
+    "mouth",
+    "grade",
+    "robin",
+    "shirt",
+    "third",
+    "march",
+    "story",
+    "wrote",
+    "lunch",
+    "world",
+    "links",
+    "along",
+    "smith",
+    "click",
+    "movie",
+    "teach",
+    "still",
+    "title",
+    "jimmy",
+    "agent",
+    "unity",
+    "debut",
+    "album",
+    "booth",
+    "label",
+    "minus",
+    "proud",
+    "earth",
+    "table",
+    "basis",
+    "audit",
+    "south",
+    "acute",
+    "woman",
+    "false",
+    "super",
+    "would",
+    "enemy",
+    "times",
+    "drove",
+    "tough",
+    "wheel",
+    "light",
+    "argue",
+    "array",
+    "large",
+    "truck",
+    "tight",
+    "enjoy",
+    "sixty",
+    "young",
+    "noted",
+    "known",
+    "serve",
+    "there",
+    "learn",
+    "sleep",
+    "usual",
+    "magic",
+    "quick",
+    "fresh",
+    "begin",
+    "clean",
+    "radio",
+    "broke",
+    "taken",
+    "thing",
+    "wrong",
+    "hotel",
+    "touch",
+    "maria",
+    "frank",
+    "empty",
+    "voice",
+    "smoke",
+    "raise",
+    "offer",
+    "stick",
+    "break",
+    "fault",
+    "fleet",
+    "glass",
+    "clock",
+    "billy",
+    "total",
+    "waste",
+    "virus",
+    "refer",
+    "alter",
+    "plant",
+    "share",
+    "brief",
+    "fraud",
+    "music",
+    "mouse",
+    "scale",
+    "plane",
+    "pitch",
+    "about",
+    "ought",
+    "tired",
+    "loose",
+    "alarm",
+    "media",
+    "noise",
+    "index",
+    "forth",
+    "moral",
+    "shall",
+    "vital",
+    "cover",
+    "shock",
+    "print",
+    "never",
+    "arise",
+    "stand",
+    "below",
+    "crown",
+    "score",
+    "prize",
+    "round",
+    "apple",
+    "plate",
+    "dozen",
+    "stuck",
+    "texas",
+    "tried",
+    "smart",
+    "harry",
+    "error",
+    "among",
+    "thick",
+    "fifty",
+    "apply",
+    "green",
+    "chair",
+    "begun",
+    "minor",
+    "shown",
+    "suite",
+    " dealt",
+    "basic",
+];
+
+let container = document.querySelector(".container");
+let winScreen = document.querySelector(".win-screen");
+let submitButton = document.querySelector(".submit");
+let inputCount, tryCount, inputRow;
+let backSpaceCount = 0;
+let randomWord, finalWord;
+
+const isTouchDevice = () => {
+    try {
+        document.createEvent("TouchEvent");
+        return true;
+    } catch (e) {
+        return false;
+    }
+};
+
+//Initial Setup
+const startGame = async () => {
+    winScreen.classList.add("hide");
+    container.innerHTML = "";
+    inputCount = 0;
+    successCount = 0;
+    tryCount = 0;
+    finalWord = "";
+
+    //Creating the grid
+    for (let i = 0; i < 6; i++) {
+        let inputGroup = document.createElement("div");
+        inputGroup.classList.add("input-group");
+        for (let j = 0; j < 5; j++) {
+            //Disabled all boxes by default
+            inputGroup.innerHTML += `<input type="text" class="input-box" onkeyup="checker(event)" maxlength="1" disabled>`;
+        }
+        await container.appendChild(inputGroup);
+    }
+    inputRow = document.querySelectorAll(".input-group");
+    inputBox = document.querySelectorAll(".input-box");
+    updateDivConfig(inputRow[tryCount].firstChild, false);
+    randomWord = getRandom();
+    console.log(randomWord);
+};
+
+//Gets random word
+const getRandom = () => words[Math.floor(Math.random() * words.length)].toUpperCase();
+
+const updateDivConfig = (element, disabledStatus) => {
+    element.disabled = disabledStatus;
+    if (!disabledStatus) {
+        element.focus();
+    }
+};
+
+const checker = async (e) => {
+    let value = e.target.value.toUpperCase();
+    //disables current input box
+    updateDivConfig(e.target, true);
+    if (value.length == 1) {
+        if (inputCount <= 4 && e.key != "Backspace") {
+            //Attach the letter to the final word
+            finalWord += value;
+            if (inputCount < 4) {
+                //enable next box
+                updateDivConfig(e.target.nextSibling, false);
+            }
+        }
+        inputCount += 1;
+    } else if (value.length == 0 && e.key == "Backspace") {
+        finalWord = finalWord.substring(0, finalWord.length - 1);
+        if (inputCount == 0) {
+            updateDivConfig(e.target, false);
+            return false;
+        }
+        updateDivConfig(e.target, true);
+        e.target.previousSibling.value = "";
+        updateDivConfig(e.target.previousSibling, false);
+        inputCount = -1;
+    }
+};
+
+window.addEventListener("keyup", (e) => {
+    if (inputCount > 4) {
+        if (isTouchDevice()) {
+            submitButton.classList.remove("hide");
+        }
+        if (e.key == "Enter") {
+            validateWord();
+        } else if (e.key == "Backspace") {
+            inputRow[tryCount].lastChild.value = "";
+            finalWord = finalWord.substring(0, finalWord.length - 1);
+            updateDivConfig(inputRow[tryCount].lastChild, false);
+            inputCount -= 1;
+        }
+    }
+});
+
+//Comparison Logic
+const validateWord = async () => {
+    if (isTouchDevice()) {
+        submitButton.classList.add("hide");
+    }
+    let failed = false;
+    let currentInputs = inputRow[tryCount].querySelectorAll(".input-box");
+    //Check if input is a valid English word
+    await fetch(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${finalWord}`
+    ).then((response) => {
+        if (response.status == "404") {
+            console.clear();
+            alert("Please Enter Valid Word");
+            failed = true;
+        }
+    });
+    if (failed) {
+        return false;
+    }
+    let successCount = 0;
+    let successLetters = "";
+    //Compares both words
+    for (let i in randomWord) {
+        if (finalWord[i] == randomWord[i]) { //alphabet is guessed at correct position
+            currentInputs[i].classList.add("correct");
+            successCount += 1;
+            successLetters += randomWord[i];
+        } else if (randomWord.includes(finalWord[i]) && !successLetters.includes(finalWord[i])) {
+            //alphabet is present in word but is at wrong position
+            currentInputs[i].classList.add("exists");
+        } else {
+            currentInputs[i].classList.add("incorrect");
+        }
+    }
+    tryCount += 1;
+    //If word is guessed
+    if (successCount == 5) {
+        //Displays the win banner
+        setTimeout(() => {
+            winScreen.classList.remove("hide");
+            winScreen.innerHTML = ` <span>Total guesses: ${tryCount}</span>
+                                    <button onclick="startGame()">New Game</button>`;
+        }, 1000);
+    } else {
+        //Unsuccessful attempt
+        inputCount = 0;
+        finalWord = "";
+        if (tryCount == 6) {
+            //All attempts exhausted
+            tryCount = 0;
+            winScreen.classList.remove("hide");
+            winScreen.innerHTML = ` <span>You lose</span>
+                                    <button onclick="startGame()">New Game</button>`;
+            return false;
+        }
+        updateDivConfig(inputRow[tryCount].firstChild, false);
+    }
+    inputCount = 0;
+};
+
+window.onload = startGame();
